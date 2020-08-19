99f5a08c4732d207f273a41fca1f3b12b9de958b
Fabian Streitel <streitel@cqse.eu>
2020-08-19 14:57:13 +0200

    read in coverage file

--- teamscaleupload/src/main/java/com/cqse/teamscaleupload/TeamscaleUpload.java
+++ teamscaleupload/src/main/java/com/cqse/teamscaleupload/TeamscaleUpload.java
@@ -1,8 +1,16 @@
 package com.cqse.teamscaleupload;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TeamscaleUpload {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        Path coverageFile = Paths.get("/home/k/proj/teamscale-upload/teamscaleupload/test.simple");
+        String reportContent = new String(Files.readAllBytes(coverageFile));
+        String project = "cgeo";
+        System.out.println("cov:\n" + reportContent);
     }
 }
