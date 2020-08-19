dad5f3a7b10008885f00b35a75eb1e8786731dc0
Fabian Streitel <streitel@cqse.eu>
2020-08-19 15:51:46 +0200

    read most inputs from command line

--- teamscaleupload/src/main/java/com/cqse/teamscaleupload/TeamscaleUpload.java
+++ teamscaleupload/src/main/java/com/cqse/teamscaleupload/TeamscaleUpload.java
@@ -1,5 +1,10 @@
 package com.cqse.teamscaleupload;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,11 +20,56 @@ import okhttp3.Response;
 
 public class TeamscaleUpload {
 
+    private static class Input {
+        public final String project;
+        public final String username;
+        public final String accessKey;
+        public final String partition;
+
+        private Input(Namespace namespace) {
+            this.project = namespace.getString("project");
+            this.username = namespace.getString("user");
+            this.accessKey = namespace.getString("accesskey");
+            this.partition = namespace.getString("partition");
+        }
+    }
+
+    private static Input parseArguments(String[] args) {
+        ArgumentParser parser = ArgumentParsers.newFor("teamscale-upload").build()
+                .defaultHelp(true)
+                .description("Upload coverage, findings, ... to Teamscale.");
+
+        parser.addArgument("-p", "--project").type(String.class).metavar("PROJECT").required(true)
+                .help("The project ID or alias (NOT the project name!) to which to upload the data.");
+        parser.addArgument("-u", "--user").type(String.class).metavar("USER").required(true)
+                .help("The username used to perform the upload. Must have the 'Perform External Uploads' permission for the given Teamscale project.");
+        parser.addArgument("-a", "--accesskey").type(String.class).metavar("ACCESSKEY").required(true)
+                .help("The IDE access key of the given user. Can be retrieved in Teamscale under Admin > Users.");
+        parser.addArgument("-t", "--partition").type(String.class).metavar("PARTITION").required(true)
+                .help("The partition into which the data is inserted in Teamscale." +
+                        " Successive uploads into the same partition will overwrite the data" +
+                        " previously inserted there, so use different partitions if you'd instead" +
+                        " like to merge data from different sources (e.g. one for Findbugs findings" +
+                        " and one for JaCoCo coverage).");
+
+        Namespace namespace;
+        try {
+            namespace = parser.parseArgs(args);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+            return null;
+        }
+
+        return new Input(namespace);
+    }
+
     public static void main(String[] args) throws IOException {
+        Input input = parseArguments(args);
+
         Path coverageFile = Paths.get("/home/k/proj/teamscale-upload/teamscaleupload/test.simple");
-        String project = "cgeo";
-        String userName = "msailer";
-        String accessKey = "7mmMTVbuQIWkwZlF0mHHoZmG9AR5RwOW";
+        String userName = input.username;
+        String accessKey = input.accessKey;
 
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
@@ -28,9 +78,9 @@ public class TeamscaleUpload {
                 .build();
 
         HttpUrl url = new HttpUrl.Builder().scheme("https").host("demo.teamscale.com")
-                .addPathSegments("api/projects").addPathSegment(project).addPathSegments("external-analysis/session/auto-create/report")
+                .addPathSegments("api/projects").addPathSegment(input.project).addPathSegments("external-analysis/session/auto-create/report")
                 .addQueryParameter("t", "master:HEAD")
-                .addQueryParameter("partition", "part")
+                .addQueryParameter("partition", input.partition)
                 .addQueryParameter("format", "SIMPLE")
                 .build();
 
