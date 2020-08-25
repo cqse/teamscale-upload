c57d8c62de9003478068b84437afe0d32b9e1944
Fabian Streitel <streitel@cqse.eu>
2020-08-25 21:54:05 +0200

    remove debug statements

---
+++ build-windows.bat
@@ -0,0 +1,6 @@
+rem calling this is required so cl.exe is on the path when native-image tries to compile our exe
+rem we can't do this from the travis bash script as the env variables set by this bat file are not
+rem propagated to the shell environment
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+./mvnw.cmd clean package
+
--- travis-build-windows.bat
+++
@@ -1,9 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-
-echo -----------------
-set
-echo -----------------
-where.exe cl.exe
-echo -----------------
-./mvnw.cmd clean package
-
