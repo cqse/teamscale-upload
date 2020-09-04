f544c62d370f682c3b26958d91f6f766b1ec7627
Fabian Streitel <streitel@cqse.eu>
2020-09-04 11:28:27 +0200

    add further tests

--- src/test/java/com/cqse/teamscaleupload/NativeImageIT.java
+++ src/test/java/com/cqse/teamscaleupload/NativeImageIT.java
@@ -2,34 +2,70 @@ package com.cqse.teamscaleupload;
 
 import com.cqse.teamscaleupload.autodetect_revision.ProcessUtils;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Runs the Maven-generated native image in different scenarios.
+ * To run this test locally, you must provide the `build` user's access key for
+ * https://demo.teamscale.com in the environment variable `ACCESS_KEY`.
  */
 public class NativeImageIT {
 
     @Test
     public void wrongAccessKey() {
         ProcessUtils.ProcessResult result = runUploader(new Arguments().withAccessKey("wrong-accesskey_"));
-        assertThat(result.exitCode).isNotZero();
-        assertThat(result.stdoutAndStdErr).contains("You provided incorrect credentials");
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result.exitCode).isNotZero();
+        softly.assertThat(result.stdoutAndStdErr).contains("You provided incorrect credentials");
+        softly.assertAll();
     }
 
     @Test
     public void wrongUser() {
         ProcessUtils.ProcessResult result = runUploader(new Arguments().withUser("wrong-user_"));
-        assertThat(result.exitCode).isNotZero();
-        assertThat(result.stdoutAndStdErr).contains("You provided incorrect credentials");
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result.exitCode).isNotZero();
+        softly.assertThat(result.stdoutAndStdErr).contains("You provided incorrect credentials");
+        softly.assertAll();
     }
 
     @Test
     public void wrongProject() {
         ProcessUtils.ProcessResult result = runUploader(new Arguments().withProject("wrong-project_"));
-        assertThat(result.exitCode).isNotZero();
-        assertThat(result.stdoutAndStdErr).contains("The project").contains("does not seem to exist in Teamscale");
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result.exitCode).isNotZero();
+        softly.assertThat(result.stdoutAndStdErr).contains("The project").contains("does not seem to exist in Teamscale");
+        softly.assertAll();
+    }
+
+    @Test
+    public void patternMatchesNothing() {
+        ProcessUtils.ProcessResult result = runUploader(new Arguments().withPattern("**/matches.nothing"));
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result.exitCode).isNotZero();
+        softly.assertThat(result.stdoutAndStdErr).contains("Could not find any files to upload");
+        softly.assertAll();
+    }
+
+    @Test
+    public void insufficientPermissions() {
+        ProcessUtils.ProcessResult result = runUploader(new Arguments()
+                .withUser("has-no-permissions").withAccessKey("SU2nfdkpcsoOXK2zDVf2DLEQiDaMD8fM"));
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result.exitCode).isNotZero();
+        softly.assertThat(result.stdoutAndStdErr).contains("is not allowed to upload data to the Teamscale project");
+        softly.assertAll();
+    }
+
+    @Test
+    public void successfulUpload() {
+        ProcessUtils.ProcessResult result = runUploader(new Arguments());
+        assertThat(result.exitCode)
+                .describedAs("Stderr and stdout: " + result.stdoutAndStdErr)
+                .isZero();
     }
 
     private static String getAccessKeyFromCi() {
@@ -48,6 +84,11 @@ public class NativeImageIT {
         private String format = "simple";
         private String partition = "NativeImageIT";
         private String pattern = "**.simple";
+
+        public Arguments withPattern(String pattern) {
+            this.pattern = pattern;
+            return this;
+        }
 
         public Arguments withAccessKey(String accessKey) {
             this.accessKey = accessKey;
