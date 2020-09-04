bf56c70e8575a1eed684b1d48c345c3f97f23849
Fabian Streitel <streitel@cqse.eu>
2020-09-04 10:44:19 +0200

    add and use maven failsafe

---
+++ src/test/java/com/cqse/teamscaleupload/FooIT.java
@@ -0,0 +1,12 @@
+package com.cqse.teamscaleupload;
+
+import org.junit.jupiter.api.Test;
+
+public class FooIT {
+
+    @Test
+    public void testBla() {
+        throw new RuntimeException("fail");
+    }
+
+}
