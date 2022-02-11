826c34377bbea32200b25c21b7cff7eff84b7bc8
rhein <rhein@cqse.eu>
2022-02-11 07:27:52 +0100

    make bat script powershell compatible

--- build-windows.bat
+++ build-windows.bat
@@ -3,13 +3,8 @@ rem calling this is required so cl.exe is on the path when native-image tries to
 rem we can't do this from the travis bash script as the env variables set by this bat file are not
 rem propagated to the shell environment
 
-
-rem If the vcvarsall.bat file does not exist, it is maybe here
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-
-
+rem There are different potential paths for the vcvarsall.bat file (on the build machine it is apparently the "Enterprise" path)
 for %%f in (
-rem There are different potential paths for the file (on the build machine it is apparently the "Enterprise" path)
         "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
         "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
        ) do (
