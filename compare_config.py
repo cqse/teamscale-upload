54b37d60ae9cd3aafd10c48a441995369c92170d
David Buderus <buderus@cqse.eu>
2023-09-27 11:04:09 +0200

    TS-35768 Added more missing reflection calls

--- compare_config.py
+++ compare_config.py
@@ -4,6 +4,8 @@ import os
 NEW_CONFIG_FOLDER="config-dir"
 CURRENT_CONFIG_FOLDER="src/main/resources/META-INF/native-image"
 
+PREFIX_FILTER = ["org.apache.maven"]
+
 def compare(file_name, new_config_folder, current_config_folder):
 
     with open(to_absolute_path(current_config_folder + "/" + file_name)) as current_config_file:
@@ -15,6 +17,9 @@ def compare(file_name, new_config_folder, current_config_folder):
     print(file_name)
     print("="*10)
     for entry in new_config:
+        if filter_out(entry):
+            continue
+
         if entry not in current_config:
             print(json.dumps(entry))
     print("\n")
@@ -23,6 +28,17 @@ def compare(file_name, new_config_folder, current_config_folder):
 def to_absolute_path(relative_path):
     absolute_path = os.path.dirname(__file__)
     return os.path.join(absolute_path, relative_path)
+
+
+def filter_out(reflection_entry):
+    if "name" not in reflection_entry:
+        return False
+    
+    for prefix in PREFIX_FILTER:
+        if reflection_entry["name"].startswith(prefix):
+            return True
+
+    return False
 
 
 for file in ("jni-config.json", "proxy-config.json", "reflect-config.json"):
